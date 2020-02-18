fe708a57de750d0cbd339ff18a63575099874296
kwizl <karis.njoroge@gmail.com>
2020-02-18 21:52:00 +0300

    Changed the for loops to each loops

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,15 +3,12 @@ def bubble_sort(arr)
 
   return arr if size == 1
 
-  loop do
-    state = false
+  arr.each do
     (size - 1).times do |i|
       if arr[i] > arr[i + 1]
-        state = true
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
       end
     end
-    break unless state
   end
   print arr.to_a
   puts
@@ -22,28 +19,21 @@ def bubble_sort_by(arr)
 
   return arr if size == 1
 
-  for i in 0 .. (size - 1)
-    for k in 1 .. size - i - 1
-      if yield(arr[k - 1], arr[k]) > 0
-        arr[k - 1], arr[k] = arr[k], arr[k - 1]
+  arr.each do
+    (size - 1).times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
       end
     end
   end
+
   print arr.to_a
   puts
 end
 
-bubble_sort_by([12, 2, 1]) do |left, right|
-  left - right
-end
-
-bubble_sort_by(["c", "a", "b"]) do |left, right|
-  left[0].ord - right[0].ord
-end
-
-bubble_sort_by(["sada", "sacaa", "sad"]) do |left, right|
-  left.length - right.length
-end
-
 test = [6, 4, 5, 3, 2]
 bubble_sort(test)
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
