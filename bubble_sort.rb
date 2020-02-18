a7a002022cb3dc5a3a2d7084875e5dac999f4058
alfred noble <ezakaphil@gmail.com>
2020-02-18 17:52:52 +0100

    implement bubble sort using the yield method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,8 +11,8 @@ def bubble_sort(arr)
       state = false  
       (size-1).times do |i|
         if arr[i] > arr[i+1]
-          arr[i], arr[i+1] = arr[i+1], arr[i]
           state = true
+          arr[i], arr[i+1] = arr[i+1], arr[i]
         end
       end
       break if not state 
@@ -23,6 +23,40 @@ def bubble_sort(arr)
 
   print arr.to_a
   puts
+end
+
+def bubble_sort_by(arr)
+  
+  size = arr.length
+
+  if (size == 1)
+
+    return arr
+
+  else
+    for i in 0 .. (size-1) do
+      for k in 1 .. size-i-1 do
+        if (yield(arr[k-1], arr[k]) > 0)
+          arr[k-1], arr[k] = arr[k], arr[k-1]
+        end
+      end
+    end
+    arr
+  end
+  print arr.to_a
+  puts
+end
+
+bubble_sort_by([12, 2, 1]) do|left, right|
+  left - right
+end
+
+bubble_sort_by(["c", "a", "b"]) do|left, right|
+  left[0].ord - right[0].ord
+end
+
+bubble_sort_by(["sada", "sacaa", "sad"]) do|left, right|
+  left.length - right.length
 end
 
 test = [6, 4, 5, 3, 2]
