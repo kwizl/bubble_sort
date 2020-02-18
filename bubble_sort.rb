381362d10947c038f89dee35e084d4c0c32d4e4b
alfred noble <ezakaphil@gmail.com>
2020-02-18 19:12:16 +0100

    add gemfile and install rubocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,61 +1,47 @@
 def bubble_sort(arr)
-
   size = arr.length
 
-  if (size == 1)
+  return arr if size == 1
 
-    return arr
-
-  else
-    loop do
-      state = false  
-      (size-1).times do |i|
-        if arr[i] > arr[i+1]
-          state = true
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-        end
+  loop do
+    state = false
+    (size - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        state = true
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
       end
-      break if not state 
     end
-
-    arr
+    break unless state
   end
-
   print arr.to_a
   puts
 end
 
 def bubble_sort_by(arr)
-  
   size = arr.length
 
-  if (size == 1)
+  return arr if size == 1
 
-    return arr
-
-  else
-    for i in 0 .. (size-1) do
-      for k in 1 .. size-i-1 do
-        if (yield(arr[k-1], arr[k]) > 0)
-          arr[k-1], arr[k] = arr[k], arr[k-1]
-        end
+  for i in 0 .. (size - 1)
+    for k in 1 .. size - i - 1
+      if yield(arr[k - 1], arr[k]) > 0
+        arr[k - 1], arr[k] = arr[k], arr[k - 1]
       end
     end
-    arr
   end
   print arr.to_a
   puts
 end
 
-bubble_sort_by([12, 2, 1]) do|left, right|
+bubble_sort_by([12, 2, 1]) do |left, right|
   left - right
 end
 
-bubble_sort_by(["c", "a", "b"]) do|left, right|
+bubble_sort_by(["c", "a", "b"]) do |left, right|
   left[0].ord - right[0].ord
 end
 
-bubble_sort_by(["sada", "sacaa", "sad"]) do|left, right|
+bubble_sort_by(["sada", "sacaa", "sad"]) do |left, right|
   left.length - right.length
 end
 
